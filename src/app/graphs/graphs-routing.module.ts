abae5edd35327df64f9942beb5970b6918b6d120
afrivera <andresrfelip@hotmail.com>
2022-06-18 14:50:58 -0500

    config project routes

--- src/app/graphs/graphs-routing.module.ts
+++ src/app/graphs/graphs-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BarsComponent } from './pages/bars/bars.component';
+import { DoubleBarsComponent } from './pages/double-bars/double-bars.component';
+import { DoughnutComponent } from './pages/doughnut/doughnut.component';
+import { DoughnutHttpComponent } from './pages/doughnut-http/doughnut-http.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'bar', component: BarsComponent },
+      { path: 'double-bar', component: DoubleBarsComponent },
+      { path: 'doughnut', component: DoughnutComponent },
+      { path: 'doughnut-http', component: DoughnutHttpComponent },
+      { path: '**', redirectTo: 'bar' }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
